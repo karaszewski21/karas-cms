fdcfcd62642d768b159645d1e863271a50ec49a6
Patryk Karaszewski <karaszewski21@gmail.com>
2020-10-02 15:00:36 +0200

    add quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const Quiz = require("../models/quiz");
 
 /* GET home page. */
+
 router.get("/", (req, res) => {
-  res.render("quiz", { title: "Quiz" });
+  const show = !req.session.quiz;
+  Quiz.find((err, data) => {
+    sumVote = data.reduce((acc, data) => {
+      return (acc += data.vote);
+    });
+
+    res.render("quiz", { title: "Quiz", data, show, sumVote });
+  });
+});
+
+router.post("/", (req, res) => {
+  const quizId = req.body.quiz;
+
+  Quiz.findOne({ _id: quizId }, (err, data) => {
+    data.vote = data.vote + 1;
+    data.save((err) => {
+      req.session.quiz = 1;
+      res.redirect("/quiz");
+    });
+  });
 });
 
 module.exports = router;
