743c8de92877c2cf49ec4dcc5c4096fe64a3f63c
Patryk Karaszewski <karaszewski21@gmail.com>
2020-10-02 07:07:44 +0200

    news filter front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const newsModel = require("../models/news");
 
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("news", { title: "News" });
+  const search = req.query.search || "";
+
+  const findNews = newsModel
+    .find({ title: new RegExp(search.trim(), "i") })
+    .sort({ createdDate: -1 });
+
+  findNews.exec((err, news) => {
+    res.render("news", { title: "News", news, search });
+  });
 });
 
 module.exports = router;
